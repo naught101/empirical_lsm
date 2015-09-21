4fb4ee0f5d470fe03ae4f90782abde5497643bd7
naught101 <naught101@naught101.org>
2015-09-21 13:22:53 +1000

    partial conversion to xray

--- run_models.py
+++ run_models.py
@@ -30,7 +30,7 @@ import pickle
 
 # import pals_utils as pu
 from pals_utils.helpers import timeit, short_hash
-from pals_utils.data import pals_site_name
+from pals_utils.data import pals_site_name, pals_xray_to_df
 
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.preprocessing import PolynomialFeatures
@@ -38,8 +38,8 @@ from sklearn.decomposition import PCA
 from sklearn.pipeline import make_pipeline
 
 
-met_vars = ["SWdown", "Tair", "LWdown", "Wind", "Rainf", "PSurf", "Qair"]
-flux_vars = ["Qh", "Qle", "Rnet", "NEE"]
+MET_VARS = ["SWdown", "Tair", "LWdown", "Wind", "Rainf", "PSurf", "Qair"]
+FLUX_VARS = ["Qh", "Qle", "Rnet", "NEE"]
 
 
 #################
@@ -49,13 +49,25 @@ flux_vars = ["Qh", "Qle", "Rnet", "NEE"]
 # Timing helpers
 
 @timeit
-def fit_pipeline(pipe, X, Y):
-    pipe.fit(X, Y)
+def fit_pipeline(pipe, met_ds, flux_ds):
+    """Fit a pipeline, given a pair of xray datasets
+
+    :param pipe:
+    :param met_ds:
+    :param flux_ds:
+    """
+    if isinstance(met_ds, list):
+        # TODO: figure out list fitting
+        pass
+    met_df = pals_xray_to_df(met_ds, MET_VARS)
+    flux_df = pals_xray_to_df(flux_ds, FLUX_VARS)
+    pipe.fit(met_df, flux_df)
 
 
 @timeit
-def get_pipeline_prediction(pipe, X):
-    return(pipe.predict(X))
+def get_pipeline_prediction(pipe, met_ds):
+    met_df = pals_xray_to_df(met_ds, MET_VARS)
+    return(pipe.predict(met_df))
 
 
 # Pipeline helpers
@@ -71,13 +83,15 @@ def get_pipeline_name(pipe, suffix=None):
 # ## fit - simulate - evaluate
 # #############################
 
-def get_model_fit_path(hash):
-    return "cache/model_fits/%s.pickle" % hash
+def get_model_fit_path(fit_hash, fit_dir="cache/model_fits"):
+    if not os.path.exists(fit_dir):
+        os.mkdir(fit_dir)
+    return "%s/%s.pickle" % (fit_dir, fit_hash)
 
 
 def fit_exists(fit_id, cache):
-    if "model_fits/" + fit_id in cache:
-        return cache["model_fits"][fit_id]["fit_hash"]
+    if "/model_fits" in cache.keys() and fit_id in cache.model_fits.index:
+        return cache.model_fits[fit_id, "fit_hash"]
 
 
 def fit_model_pipeline(pipe, met_data, flux_data, name, cache, clear_cache=False):
@@ -103,9 +117,20 @@ def fit_model_pipeline(pipe, met_data, flux_data, name, cache, clear_cache=False
     else:
         _, fit_time = fit_pipeline(pipe, met_data, flux_data)
         fit_hash = short_hash(pipe)
-        cache["model_fits"][fit_id]["fit_hash"] = fit_hash
-        cache["model_fits"][fit_id]["fit_time"] = fit_time
+
+        if "/model_fits" in cache.keys():
+            model_fits = cache.model_fits
+        else:
+            model_fits = pd.DataFrame()
+
+        model_fits[fit_id, "fit_hash"] = fit_hash
+        model_fits[fit_id, "fit_time"] = fit_time
+        model_fits[fit_id, "name"] = name
+        model_fits[fit_id, "site"] = pals_site_name(met_data)
+
+        cache['model_fits'] = model_fits
         cache.flush()
+
         with open(get_model_fit_path(fit_hash), "wb") as f:
             pickle.dump(pipe, f)
 
@@ -114,13 +139,15 @@ def fit_model_pipeline(pipe, met_data, flux_data, name, cache, clear_cache=False
 
 # Simulate
 
-def get_sim_path(hash):
-    return "cache/simulations/%s.pickle" % hash
+def get_sim_path(sim_hash, sim_dir="cache/simulations"):
+    if not os.path.exists(sim_dir):
+        os.mkdir(sim_dir)
+    return "%s/%s.pickle" % (sim_dir, sim_hash)
 
 
 def sim_exists(fit_hash, cache):
-    if "simulations/" + fit_hash in cache:
-        return cache["simulations"][fit_hash]["sim_hash"]
+    if "/simulations" in cache.keys() and fit_hash in cache.simulations.index:
+        return cache.simulations[fit_hash, "sim_hash"]
 
 
 def simulate_model_pipeline(pipe, met_data, name, cache, clear_cache=False):
@@ -144,11 +171,11 @@ def simulate_model_pipeline(pipe, met_data, name, cache, clear_cache=False):
     else:
         if met_data.met is None or met_data.flux is None:
             raise KeyError("missing met or flux data")
-        sim_data, fit_time = get_pipeline_prediction(pipe, met_data)
+        sim_data, pred_time = get_pipeline_prediction(pipe, met_data)
         # TODO: If a simulation can produce more than one output for a given input, this won"t be unique. Is that ok?
         sim_hash = short_hash(sim_data)
-        cache["simulations"][fit_hash]["sim_hash"] = sim_hash
-        cache["simulations"][fit_hash]["model_predict_time"] = fit_time
+        cache.append("simulations/%s/sim_hash" % fit_hash, sim_hash)
+        cache.append("simulations/%s/predict_time" % fit_hash, pred_time)
         cache.flush()
         with open(get_model_fit_path(sim_hash), "wb") as f:
             pickle.dump(sim_data, f)
@@ -234,7 +261,7 @@ def main_fit_model(args):
     name = get_pipeline_name(pipe)
 
     met_data = xray.open_dataset(args["<metfile>"])
-    flux_data = xray.open_dataset(args["<metfile>"])
+    flux_data = xray.open_dataset(args["<fluxfile>"])
 
     if not os.path.exists("cache/"):
         os.mkdir("cache")
