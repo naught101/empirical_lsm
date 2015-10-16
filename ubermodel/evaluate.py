379bd951d7621acc7aa1bb3c0be9dfe912edcabb
naught101 <naught101@naught101.org>
2015-10-16 12:55:10 +1100

    re-imaged eval

--- ubermodel/evaluate.py
+++ ubermodel/evaluate.py
@@ -9,55 +9,36 @@ Description: PALS-style model evaluation
 """
 
 import pandas as pd
-from datetime import datetime
 
-from pals_utils.data import pals_site_name
+from pals_utils.data import pals_site_name, FLUX_VARS
 from pals_utils.stats import run_metrics
-from pals_utils.helpers import short_hash
 
 
 # Evaluate
 
-def evaluate_simulation(sim_data, flux_data, model_name, flux_vars, cache):
+def evaluate_simulation(sim_data, flux_data, name):
     """Top-level simulation evaluator.
 
-    Compares sim_data to flux_data, using standard metrics. Stores the results in an easily accessible format.
+    Compares sim_data to flux_data, using standard metrics.
 
     TODO: Maybe get model model_name from sim_data directly (this is a PITA at
           the moment, most models don't report it).
     """
 
-    eval_hash = short_hash((sim_data, flux_data))[0:7]
-    eval_time = datetime.now().isoformat()
+    eval_vars = list(set(FLUX_VARS).intersection(sim_data.data_vars)
+                                   .intersection(flux_data.data_vars))
 
-    # TODO: This currently returns "TumbaFluxnet" - would be nice to return the proper name.
-    #       Probably should be fixed in PALS.
+    metric_data = pd.DataFrame()
+    for v in eval_vars:
+        sim_v = sim_data[v].values.ravel()
+        obs_v = flux_data[v].values.ravel()
+
+        for m, val in run_metrics(sim_v, obs_v).items():
+            metric_data.ix[m, v] = val
+
     site = pals_site_name(flux_data)
 
-    index = {"eval_hash": "%s_%s" % (eval_hash, flux_vars[0])}
+    eval_path = 'source/models/{0}/{1}.csv'.format(name, site)
+    metric_data.to_csv(eval_path)
 
-    if "metric_data" in cache:
-        metric_data = cache.metric_data
-    else:
-        metric_data = pd.DataFrame([index])
-        metric_data = metric_data.set_index(list(index.keys()))
-
-    for y_var in flux_vars:
-        Y_sim = sim_data[y_var].values.ravel()
-        Y_obs = flux_data[y_var].values.ravel()
-
-        row_id = "%s_%s" % (eval_hash, y_var)
-        metric_data.ix[row_id, "eval_hash"] = eval_hash
-        metric_data.ix[row_id, "model_name"] = model_name
-        metric_data.ix[row_id, "sim_hash"] = short_hash(sim_data)
-        metric_data.ix[row_id, "site"] = site
-        metric_data.ix[row_id, "var"] = y_var
-        metric_data.ix[row_id, "eval_time"] = eval_time
-
-        for k, v in run_metrics(Y_sim, Y_obs).items():
-            metric_data.ix[row_id, k] = v
-
-    cache["metric_data"] = metric_data
-    cache.flush()
-
-    return metric_data.query("eval_hash == '%s'" % eval_hash)
+    return metric_data
