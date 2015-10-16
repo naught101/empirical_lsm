476311d44d5f373f6e7cee0dcb93fdaf714d2000
naught101 <naught101@naught101.org>
2015-10-16 18:59:39 +1100

    re-jig plots, add scatterplot

--- ubermodel/plots.py
+++ ubermodel/plots.py
@@ -13,7 +13,7 @@ import seaborn as sns
 import pandas as pd
 import os
 
-from pals_utils.data import pals_site_name, pals_xray_to_df
+from pals_utils.data import pals_site_name, pals_xray_to_df, FLUX_VARS
 
 
 def diagnostic_plots(sim_data, flux_data, name):
@@ -34,20 +34,35 @@ def diagnostic_plots(sim_data, flux_data, name):
     files = []
 
     # TODO: For variables
-    for var in ['Qh']:
-        plot_monthly_timeseries(sim_data, flux_data, var, name)
-        filename = '{0}_{1}_monthly_timeseries.png'.format(name, site)
-        plot_path = os.path.join(fig_path, filename)
-        pl.savefig(plot_path)
+    for var in FLUX_VARS:
+        data = pd.concat([pals_xray_to_df(sim_data, [var]), pals_xray_to_df(flux_data, [var])], axis=1)
+        data.columns = ['modelled', 'observed']
 
-        rel_plot_path = os.path.join('figures', filename)
-        files.append(rel_plot_path)
+        for plot in PLOTS:
+            filename = plot(data, name, var, site)
+            plot_path = os.path.join(fig_path, filename)
+            pl.savefig(plot_path)
+
+            rel_plot_path = os.path.join('figures', filename)
+            files.append(rel_plot_path)
 
     return files
 
 
-def plot_monthly_timeseries(sim_data, flux_data, var, name):
-    data = pd.concat([pals_xray_to_df(sim_data, [var]), pals_xray_to_df(flux_data, [var])], axis=1)
-    data.columns = ['modelled', 'observed']
+def plot_weekly_timeseries(data, name, var, site):
+    data.resample('1W').plot()
+    pl.title('{0}: Weekly average {1} at {2}'.format(name, var, site))
 
-    return data.plot()
+    filename = '{0}_{1}_{2}_weekly_timeseries.png'.format(name, var, site)
+    return filename
+
+
+def plot_scatter(data, name, var, site):
+    data.plot.scatter('observed', 'modelled', c='black', s=1, alpha=0.5)
+    pl.title('{0}: Scatterplot of {1} at {2}'.format(name, var, site))
+
+    filename = '{0}_{1}_{2}_scatterplot.png'.format(name, var, site)
+    return filename
+
+
+PLOTS = [plot_weekly_timeseries, plot_scatter]
