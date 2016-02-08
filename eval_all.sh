3396e3f3579777f9fc0950a2bf40262c01603127
naught101 <naught101@naught101.org>
2016-02-08 16:07:16 +1100

    Fix eval_all loop

--- eval_all.sh
+++ eval_all.sh
@@ -10,7 +10,7 @@ for m in $MODELS ; do ((i=i%N))
         scripts/eval_model.py eval $m $s ;
         scripts/eval_model.py rst-gen $m $s ;
     done
-    scripts/model_search_indexes.py model $m
+    scripts/model_search_indexes.py model $m &
 done    
 
 scripts/model_search_indexes.py all
