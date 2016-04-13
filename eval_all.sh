81190ecd85025da359bb2769c508ce7b5c5d7ca8
naught101 <naught101@naught101.org>
2016-04-13 14:10:54 +1000

    nicer eval_all script

--- eval_all.sh
+++ eval_all.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 MODELS=$@
 
-N=8
-for m in $MODELS ; do ((i=i%N))
+N=3
+for m in $MODELS ; do
+    ((i=i%N))
     ((i++==0)) && wait
+    (
     for s in `cat data/sites.txt` ; do
         # scripts/run_model.py run $m $s
         scripts/eval_model.py eval $m $s ;
         scripts/eval_model.py rst-gen $m $s ;
     done
+    )
     scripts/model_search_indexes.py model $m &
 done    
 
