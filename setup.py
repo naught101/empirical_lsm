f33d9f6545e05112cab692cab072ad83dd797d63
naught101 <naught101@naught101.org>
2016-06-18 15:52:56 +1000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ubermodel',
+      version='0.1',
+      description='Empirical land surface model',
+      author='ned haughton',
+      author_email='ned@nedhaughton.com',
+      url='https://bitbucket.org/naught101/empirical_ubermodel',
+      packages=['ubermodel'],
+     )
