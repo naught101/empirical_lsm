b2c448721924ec57828ac412a8426b498b7c344c
naught101 <naught101@naught101.org>
2016-09-28 16:14:14 +1000

    make run/eval/html steps all optional

--- scripts/offline_runs/eval_all.py
+++ scripts/offline_runs/eval_all.py
@@ -8,7 +8,7 @@ Github: https://github.com/naught101/
 Description: Runs all steps of the offline runs/ubermodel search page generator
 
 Usage:
-    eval_all.py <model>... [--sites=<sites>] [--rebuild] [--no-mp]
+    eval_all.py <model>... [--sites=<sites>] [--run] [--eval] [--html] [--rebuild] [--no-mp]
     eval_all.py (-h | --help | --version)
 
 Options:
@@ -22,27 +22,41 @@ import subprocess
 
 from scripts.offline_runs.run_model import main_run_mp
 from scripts.offline_runs.eval_model import main_eval_mp, main_rst_gen_mp
-from scripts.offline_runs.model_search_indexes import model_site_index_rst_mp, model_search_index_rst
+from scripts.offline_runs.model_search_indexes import model_site_index_rst_mp, model_search_index_rst, get_available_models
 
 
-
-def main_eval_all(names, sites, rebuild=False, no_mp=False):
+def main_eval_all(names, sites, run, evalu, html, rebuild=False, no_mp=False):
 
     # All scripts already use multiprocessing
     for name in names:
-        main_run_mp(name, sites, rebuild, no_mp)
-        main_eval_mp(name, sites, rebuild, no_mp)
-        main_rst_gen_mp(name, sites, rebuild, no_mp)
+        if run:
+            main_run_mp(name, sites, no_mp)
+        if evalu:
+            main_eval_mp(name, sites, no_mp)
+            main_rst_gen_mp(name, sites, no_mp)
 
-    model_site_index_rst_mp(names, rebuild, no_mp)
-    model_search_index_rst()
-    
-    subprocess.call('make', 'html')
+    if html:
+        model_site_index_rst_mp(names, rebuild, no_mp)
+        model_search_index_rst()
+
+        subprocess.call('make', 'html')
 
 
 def main(args):
 
-    main_eval_all(args['<model>'], args['--sites'], args['--rebuild'], args['--no-mp'])
+    if args['<model>'] == ['all']:
+        names = get_available_models()
+    else:
+        names = args['<model>']
+
+    sites = args['--sites']
+    run = args['--run']
+    evalu = args['--eval']
+    html = args['--html']
+    rebuild = args['--rebuild']
+    no_mp = args['--no-mp']
+
+    main_eval_all(names, sites, run, evalu, html, rebuild, no_mp)
 
     return
 
