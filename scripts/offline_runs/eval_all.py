e7d59d4daee6ed410aa398df519a1527ae3f8d6f
naught101 <naught101@naught101.org>
2016-10-17 16:36:09 +1100

    Add multivariate running to run_all

--- scripts/offline_runs/eval_all.py
+++ scripts/offline_runs/eval_all.py
@@ -8,7 +8,7 @@ Github: https://github.com/naught101/
 Description: Runs all steps of the offline runs/ubermodel search page generator
 
 Usage:
-    eval_all.py <model>... [--sites=<sites>] [--run] [--eval] [--html] [--rebuild] [--no-mp]
+    eval_all.py <model>... [--sites=<sites>] [--run] [--multivariate] [--eval] [--html] [--rebuild] [--no-mp]
     eval_all.py (-h | --help | --version)
 
 Options:
@@ -25,12 +25,12 @@ from scripts.offline_runs.eval_model import main_eval_mp, main_rst_gen_mp
 from scripts.offline_runs.model_search_indexes import model_site_index_rst_mp, model_search_index_rst, get_available_models
 
 
-def main_eval_all(names, sites, run, evalu, html, rebuild=False, no_mp=False):
+def main_eval_all(names, sites, run, multivariate, evalu, html, rebuild=False, no_mp=False):
 
     # All scripts already use multiprocessing
     if run:
         for name in names:
-            main_run_mp(name, sites, no_mp)
+            main_run_mp(name, sites, no_mp, multivariate)
 
     if evalu:
         for name in names:
@@ -53,12 +53,13 @@ def main(args):
 
     sites = args['--sites']
     run = args['--run']
+    multivariate = args['--multivariate']
     evalu = args['--eval']
     html = args['--html']
     rebuild = args['--rebuild']
     no_mp = args['--no-mp']
 
-    main_eval_all(names, sites, run, evalu, html, rebuild, no_mp)
+    main_eval_all(names, sites, run, multivariate, evalu, html, rebuild, no_mp)
 
     return
 
