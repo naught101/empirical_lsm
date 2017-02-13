cb187656c51cd1c19a90095da09c4543e9050919
naught101 <naught101@naught101.org>
2017-02-13 13:43:53 +1100

    put model set selection stuff into module

---
+++ ubermodel/model_sets.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File: model_sets.py
+Author: naught101
+Email: naught101@email.com
+Description: Gets named sets of models, for use in various scripts
+"""
+
+from itertools import chain, combinations
+
+
+def powerset(iterable, start=0):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(start, len(s) + 1))
+
+
+def get_combo_model_names():
+
+    opts = ['W', 'dT', 'dQ', '_lS30d', '_lR30d', '_lH10d']
+    res = ['243', '729']
+
+    combos = powerset(opts, 0)
+    names = []
+    for c in combos:
+        for r in res:
+            names += ['STH' + ''.join(c) + '_km' + r]
+
+    return names
+
+
+def get_model_set(model_set):
+    """Gets pre-defined model sets
+
+    :model_set: TODO
+    :returns: TODO
+
+    """
+
+    lags = ['30min', '1h', '2h', '6h', '12h', '1d', '2d', '7d', '10d', '30d', '60d', '90d', '180d']
+    lag_fmts = ['STH_l%%s%s_km243' % l for l in lags]
+
+    base_models = ['S_lin', 'ST_lin', 'STH_km27', 'STH_km243']
+
+    model_sets = {
+        "Added variables": ['S_lin', 'ST_lin', 'STH_km27', 'STH_km243',
+                            'STHL_km27', 'STHW_km27', 'STHR_km27',
+                            'STHL_km243', 'STHW_km243', 'STHR_km243'],
+
+        "Increasing articulation": ['S_lin', 'ST_lin', 'STH_km27', 'STH_km243',
+                                    'STH_km729', 'STH_km2187'],
+
+        "Lagged SWdown": base_models + [f % 'S' for f in lag_fmts],
+        "Lagged Tair":   base_models + [f % 'T' for f in lag_fmts],
+        "Lagged RelHum": base_models + [f % 'H' for f in lag_fmts],
+        "Lagged LWdown": base_models + ['STHL_km243'] + [f % 'L' for f in lag_fmts],
+        "Lagged Rainf":  base_models + ['STHR_km243'] + [f % 'R' for f in lag_fmts],
+        "Lagged Wind":   base_models + ['STHW_km243'] + [f % 'W' for f in lag_fmts],
+
+        "Combo models": ["S_lin", "ST_lin", "STH_km27"] + get_combo_model_names()
+    }
+
+    return model_sets[model_set]
