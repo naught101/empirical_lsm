f92520054215a1dd5e83bd91fe23749bbf87c4c6
naught101 <naught101@gmail.com>
2017-08-02 15:03:15 +1000

    Split train_test_data getter into two

--- empirical_lsm/data.py
+++ empirical_lsm/data.py
@@ -99,52 +99,71 @@ def get_multimodel_wide_df(site, names, variables):
         raise Exception("WTF is variables? %s" % type(variables))
 
 
-def get_train_test_data(site, met_vars, flux_vars, use_names, fix_closure=True):
+def get_train_data(train_sites, met_vars, flux_vars, use_names, qc=True, fix_closure=True):
+
+    met_train = get_multisite_met_df(train_sites, variables=met_vars, qc=qc, name=use_names)
+    flux_train = get_multisite_flux_df(train_sites, variables=flux_vars, qc=qc, name=use_names, fix_closure=fix_closure)
+
+    return dict(
+        train_sites=train_sites,
+        met_vars=met_vars,
+        flux_vars=flux_vars,
+        fix_closure=fix_closure,
+        met_train=met_train,
+        flux_train=flux_train)
+
+
+def get_test_data(site, met_vars, use_names, qc=False, fix_closure=True):
+
+    # We use gap-filled data for the testing period, or the model fails.
+    met_test_xr = get_met_data(site)[site]
+    met_test = pals_xr_to_df(met_test_xr, variables=met_vars, qc=qc, name=use_names)
+
+    return dict(
+        site=site,
+        met_vars=met_vars,
+        fix_closure=fix_closure,
+        met_test=met_test,
+        met_test_xr=met_test_xr)
+
+
+def get_train_test_data(site, met_vars, flux_vars, use_names, qc=True, fix_closure=True):
+    """Gets training and testing data, PLUMBER style (leave one out)
+
+    Set the training set using pals.data.set_config(['datasets', 'train'])"""
 
     if site == 'debug':
         train_sites = ['Amplero']
         test_site = 'Tumba'
 
         # Use non-quality controlled data, to ensure there's enough to train
-        met_train = get_multisite_met_df(train_sites, variables=met_vars, name=use_names)
-        flux_train = get_multisite_flux_df(train_sites, variables=flux_vars, name=use_names, fix_closure=fix_closure)
-
-        met_test_xr = get_met_data(test_site)[test_site]
-        met_test = pals_xr_to_df(met_test_xr, variables=met_vars)
-
-        # met_test_xr = met_test_xr.isel(time=slice(0, 5000))
-        # met_train = met_train[0:5000]
-        # flux_train = flux_train[0:5000]
-        # met_test = met_test[0:5000]
+        qc = False
 
     else:
-        training_datasets = get_sites(get_config(['datasets', 'train']))
-        if site not in training_datasets:
+        train_sites = get_sites(get_config(['datasets', 'train']))
+        test_site = site
+        if test_site not in train_sites:
             # Running on a non-training site, train on all training sites.
-            train_sites = training_datasets
+            train_sites = train_sites
         else:
             # Running on a training site, so leave it out.
-            train_sites = [s for s in training_datasets if s != site]
+            train_sites = [s for s in train_sites if s != test_site]
         print("Training with {n} datasets".format(n=len(train_sites)))
 
-        met_train = get_multisite_met_df(train_sites, variables=met_vars, qc=True, name=use_names)
+    train_dict = get_train_data(train_sites, met_vars, flux_vars, use_names=use_names,
+                                qc=qc, fix_closure=fix_closure)
 
-        # We use gap-filled data for the testing period, or the model fails.
-        met_test_xr = get_met_data(site)[site]
-        met_test = pals_xr_to_df(met_test_xr, variables=met_vars)
+    test_dict = get_test_data(test_site, met_vars, use_names=use_names, qc=qc)
 
-        flux_train = get_multisite_flux_df(train_sites, variables=flux_vars, qc=True, name=use_names)
+    train_test_data = train_dict
+    train_test_data.update(test_dict)
+    train_test_data['site'] = site
 
-    return dict(
-        site=site,
-        train_sites=train_sites,
-        met_vars=met_vars,
-        flux_vars=flux_vars,
-        fix_closure=fix_closure,
-        met_train=met_train,
-        met_test=met_test,
-        met_test_xr=met_test_xr,
-        flux_train=flux_train)
+    with open('new_data.txt', 'w') as f:
+        for k in sorted(train_test_data.keys()):
+            print(k, ':\n', train_test_data[k], file=f)
+
+    return train_test_data
 
 
 def get_lagged_df(df, lags=['30min', '2h', '6h', '2d', '7d', '30d', '90d']):
